e508946081103188b8cbe521f567aeafd8292e5b
suzhenghui <343196323@qq.com>
2023-12-18 09:56:33 +0800

    ✨ feat: 容器

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -1,4 +1,10 @@
-import {createStore} from 'vuex'
+/*
+ * @Author: suzhenghui 343196323@qq.com
+ * @Date: 2023-12-02 20:05:53
+ * @LastEditors: suzhenghui 343196323@qq.com
+ * @Description: 容器
+ */
+import { createStore } from 'vuex'
 
 export default createStore({
     state: {
