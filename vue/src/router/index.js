4b82864debd336b4eae7e85e505f03218ca53c6e
suzhenghui <343196323@qq.com>
2023-12-18 09:34:59 +0800

    ✨ feat: 路由

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,27 +1,33 @@
+/*
+ * @Author: suzhenghui 343196323@qq.com
+ * @Date: 2023-11-29 20:05:12
+ * @LastEditors: suzhenghui 343196323@qq.com
+ * @Description: 页面路由
+ */
 import Layout from '../layout/Layout.vue'
-import {createRouter, createWebHistory} from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 
 export const constantRoutes = [
-    {path: '/Login', name: 'Login', component: () => import("@/views/Login")},
+    { path: '/Login', name: 'Login', component: () => import("@/views/Login") },
     {
         path: '/Layout', name: 'Layout', component: Layout, children: [
             //
-            {path: '/home', name: 'Home', component: () => import("@/views/Home")},
-            {path: '/stuInfo', name: 'StuInfo', component: () => import("@/views/StuInfo")},
-            {path: '/dormManagerInfo', name: 'DormManagerInfo', component: () => import("@/views/DormManagerInfo")},
-            {path: '/buildingInfo', name: 'BuildingInfo', component: () => import("@/views/BuildingInfo")},
-            {path: '/roomInfo', name: 'RoomInfo', component: () => import("@/views/RoomInfo")},
-            {path: '/noticeInfo', name: 'NoticeInfo', component: () => import("@/views/NoticeInfo")},
-            {path: '/adjustRoomInfo', name: 'AdjustRoomInfo', component: () => import("@/views/AdjustRoomInfo")},
-            {path: '/repairInfo', name: 'RepairInfo', component: () => import("@/views/RepairInfo")},
-            {path: '/visitorInfo', name: 'VisitorInfo', component: () => import("@/views/VisitorInfo")},
+            { path: '/home', name: 'Home', component: () => import("@/views/Home") },
+            { path: '/stuInfo', name: 'StuInfo', component: () => import("@/views/StuInfo") },
+            { path: '/dormManagerInfo', name: 'DormManagerInfo', component: () => import("@/views/DormManagerInfo") },
+            { path: '/buildingInfo', name: 'BuildingInfo', component: () => import("@/views/BuildingInfo") },
+            { path: '/roomInfo', name: 'RoomInfo', component: () => import("@/views/RoomInfo") },
+            { path: '/noticeInfo', name: 'NoticeInfo', component: () => import("@/views/NoticeInfo") },
+            { path: '/adjustRoomInfo', name: 'AdjustRoomInfo', component: () => import("@/views/AdjustRoomInfo") },
+            { path: '/repairInfo', name: 'RepairInfo', component: () => import("@/views/RepairInfo") },
+            { path: '/visitorInfo', name: 'VisitorInfo', component: () => import("@/views/VisitorInfo") },
             //
-            {path: '/myRoomInfo', name: 'MyRoomInfo', component: () => import("@/views/MyRoomInfo")},
-            {path: '/applyRepairInfo', name: 'ApplyRepairInfo', component: () => import("@/views/ApplyRepairInfo")},
-            {path: '/applyChangeRoom', name: 'ApplyChangeRoom', component: () => import("@/views/ApplyChangeRoom")},
+            { path: '/myRoomInfo', name: 'MyRoomInfo', component: () => import("@/views/MyRoomInfo") },
+            { path: '/applyRepairInfo', name: 'ApplyRepairInfo', component: () => import("@/views/ApplyRepairInfo") },
+            { path: '/applyChangeRoom', name: 'ApplyChangeRoom', component: () => import("@/views/ApplyChangeRoom") },
 
-            {path: '/selfInfo', name: 'SelfInfo', component: () => import("@/views/SelfInfo")},
+            { path: '/selfInfo', name: 'SelfInfo', component: () => import("@/views/SelfInfo") },
         ]
     },
 
