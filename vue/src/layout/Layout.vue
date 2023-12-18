0108bd22110b1c68897cbad434bb161a09179fe8
suzhenghui <343196323@qq.com>
2023-12-18 09:51:06 +0800

    🦄 refactor: 去除多余引入

--- vue/src/layout/Layout.vue
+++ vue/src/layout/Layout.vue
@@ -1,17 +1,23 @@
+<!--
+ * @Author: suzhenghui 343196323@qq.com
+ * @Date: 2023-11-25 20:42:36
+ * @LastEditors: suzhenghui 343196323@qq.com
+ * @Description: 整体布局
+-->
 <template>
   <el-container>
     <!--    侧边功能栏-->
     <el-aside width="200px">
-      <Aside/>
+      <Aside />
     </el-aside>
     <el-container>
       <!--      头部-->
-      <el-header style="height: 50px;border-bottom: 1px solid #ccc;">
-        <Header/>
+      <el-header style="height: 50px; border-bottom: 1px solid #ccc">
+        <Header />
       </el-header>
-      <el-main style="padding: 0;">
+      <el-main style="padding: 0">
         <!--        内容区域-->
-        <router-view/>
+        <router-view />
       </el-main>
     </el-container>
   </el-container>
@@ -21,17 +27,14 @@
 import Header from "@/components/Header";
 import Aside from "@/components/Aside";
 
-const {ElMessage} = require("element-plus");
-
 export default {
   name: "layout",
   components: {
     Header,
     Aside,
   },
-}
+};
 </script>
 
 <style scoped>
-
 </style>
