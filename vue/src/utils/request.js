06b70ddac24c188db0989ff0b7cd0c197797f547
suzhenghui <343196323@qq.com>
2023-12-18 09:56:47 +0800

    ✨ feat: Axios封装

--- vue/src/utils/request.js
+++ vue/src/utils/request.js
@@ -1,14 +1,22 @@
+/*
+ * @Author: suzhenghui 343196323@qq.com
+ * @Date: 2023-11-26 13:25:12
+ * @LastEditors: suzhenghui 343196323@qq.com
+ * @Description: 全局Axios请求拦截封装
+ */
 import axios from 'axios'
 
 const request = axios.create({
-    baseURL: '/api',  // 注意！！ 这里是全局统一加上了 '/api' 前缀，也就是说所有接口都会加上'/api'前缀在，页面里面写接口的时候就不要加 '/api'了，否则会出现2个'/api'，类似 '/api/api/user'这样的报错，切记！！！
+    // 全局统一加上了 '/api' 前缀，即所有接口都会加上'/api'前缀
+    // 后端就可以不用加api前缀了
+    baseURL: '/api',
     timeout: 5000
 })
 let token = '';
 // request 拦截器
 // 可以自请求发送前对请求做一些处理
-// 比如统一加token，对请求参数统一加密
-//添加一个请求拦截器
+// 统一加token，对请求参数统一加密
+/** 添加一个请求拦截器 */
 axios.interceptors.request.use(function (config) {
     let user = JSON.parse(window.sessionStorage.getItem('access-user'));
     if (user) {
@@ -24,8 +32,7 @@ axios.interceptors.request.use(function (config) {
     return Promise.reject(error);
 });
 
-// response 拦截器
-// 可以在接口响应后统一处理结果
+/**response 拦截器,在接口响应后统一处理结果*/
 request.interceptors.response.use(
     response => {
         let res = response.data;
